a8c8acb5d6c2204e645b6bf5a75145e0f1650208
Arahman75 <selfarahman01825@gmail.com>
2022-02-05 23:38:36 +0600

    true versus false

--- true-false.js
+++ true-false.js
@@ -1,7 +1,127 @@
+const age =4;
+if(age >0){
+    console.log('condition is true.')
+}
+else{
+    console.log('condition is false.')
+}
+const age =-4;
+if(age){
+    console.log('condition is true.')
+}
+else{
+    console.log('condition is false.')
+}
 const age =0;
 if(age){
     console.log('condition is true.')
 }
 else{
+    console.log('condition is false.')
+}
+const name ="Solaiman";
+if(name.length >0){
+    console.log('condition is true.')
+}
+else{
+    console.log('condition is false.')
+}
+
+const name ="Solaiman";
+if(name.length){
+    console.log('condition is true.')
+}
+else{
+    console.log('condition is false.')
+}
+
+const name ="Solaiman";
+if(name){
+    console.log('condition is true.')
+}
+else{
+    console.log('condition is false.')
+}
+
+const name ="";
+if(name){
+    console.log('condition is true.')
+}
+else{
+    console.log('condition is false.')
+}
+
+const name ="0";
+if(name){
+    console.log('condition is true.')
+}
+else{
+    console.log('condition is false.')
+}
+
+// const name ="0";
+let name;
+console.log(name);
+if(name){
+    console.log('condition is true.')
+}
+else{
+    console.log('condition is false.')
+}
+
+const name ="0";
+let name = null;
+console.log(name);
+if(name){
+    console.log('condition is true.')
+}
+else{
+    console.log('condition is false.')
+}
+
+// Falsy:
+//false
+// ""
+// null
+// NaN
+// 0
+//Undefined
+//Truthy
+//"0" ","" "
+//[]
+//{}
+//'false'
+let name = NaN;
+console.log(name);
+if(name){
+    console.log('condition is true.')
+}
+else{
+    console.log('condition is false.')
+}
+
+let name = [];
+console.log(name);
+if(name){
+    console.log('condition is true.')
+}
+else{
+    console.log('condition is false.')
+}
+
+
+let name = 0;
+if (name || name == 0) {
+    console.log('condition is true.')
+}
+else {
+    console.log('condition is false.')
+}
+
+let name = 12;
+if (name || name == 0) {
+    console.log('condition is true.')
+}
+else {
     console.log('condition is false.')
 }
