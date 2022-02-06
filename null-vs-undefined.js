906c5ab6918694ddbbd1a50beaf505364c3aa56e
Arahman75 <selfarahman01825@gmail.com>
2022-02-06 10:41:52 +0600

    null vs undefined cleared foe interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,42 @@
+let pakhi;
+console.log(pakhi);
+
+function add(num1, num2){
+    console.log(num1 + num2);
+}
+const result = add(13, 82);
+console.log(result);
+
+function add(num1, num2){
+    const even = num1 + num2;
+    return even;
+}
+const result = add(7, 23);
+console.log(result);
+
+function add(num1, num2){
+    console.log(num1,num2);
+}
+const result = add(13);
+console.log(result);
+
+function add(num1, num2 = 10){
+    console.log(num1,num2);
+}
+const result = add(13);
+console.log(result);
+
+const lover = {name:"Tanjin Tisha", phone:111222};
+console.log(lover.name);
+
+const lover = {name:"Tanjin Tisha", phone:111222};
+console.log(lover);
+
+const lover = {name:"Tanjin Tisha", phone:111222};
+console.log(lover.bf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 3, 4];
+console.log(ages[11]);
