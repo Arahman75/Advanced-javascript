2220862c63dae230201d2be977514345162838e6
Arahman75 <selfarahman01825@gmail.com>
2022-02-07 10:42:55 +0600

    added a scope

---
+++ scope.js
@@ -0,0 +1,104 @@
+function sum(first, second){
+    const result = first + second;
+    return result;
+}
+const output = sum(3, 7);
+console.log(output);
+
+function sum(first, second){
+    const result = first + second;
+    console.log(result);
+    return result;
+}
+const output = sum(7, 3);
+
+console.log(output);
+
+let bonus = 20;
+function sum(first, second){
+    const result = first + second + bonus;
+    console.log(bonus);
+    return result;
+}
+const output = sum(7, 3);
+console.log(bonus);
+console.log(output);
+
+let bonus = 20;
+function sum(first, second){
+    const result = first + second + bonus;
+    if(result > 9){
+        const mood = "happy";
+        console.log(mood);
+    }
+    return result;
+}
+const output = sum(7, 3);
+
+let bonus = 20;
+function sum(first, second){
+    const result = first + second + bonus;
+    if(result > 9){
+        const mood = "happy";
+        console.log(mood);
+    }
+    console.log(mood);
+    return result;
+    
+}
+const output = sum(7, 3);
+
+let bonus = 20;
+function sum(first, second){
+    const result = first + second + bonus;
+    if(result > 9){
+        let mood = "happy";
+        mood = "Fishy";
+        mood = "funky";
+        mood = "cranky";
+        console.log(mood);
+    }
+    console.log(mood);
+    return result;
+    
+}
+const output = sum(7, 3);
+
+let bonus = 20;
+function sum(first, second){
+    const result = first + second + bonus;
+    if(result > 9){
+        var mood = "happy";
+        mood = "Fishy";
+        mood = "funky";
+        mood = "cranky";
+        console.log(mood);
+    }
+    // console.log(mood);
+    console.log(day);
+var day = "friday";
+    return result;
+    
+}
+const output = sum(7, 3);
+
+let bonus = 20;
+function sum(first, second){
+    const result = first + second + bonus;
+    if(result > 9){
+        var mood = "happy";
+        mood = "Fishy";
+        mood = "funky";
+        mood = "cranky";
+        console.log(mood);
+    }
+    // console.log(mood);
+    
+var day = "friday";
+console.log(day);
+    return result;
+    
+}
+const output = sum(7, 3);
+
+
